735cd21efeeb3d38ce8502a16e4055e6e07247ed
urara036 <oura036@gmail.com>
2012-09-30 21:38:30 +0900

    first commit

---
+++ Refactoring11/test/com/example/AssertTest.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AssertTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		assertEquals(1+1, 3);
+	}
+
+}
